6e577a9c9cc0163ec9aa0c8a6d4bce48fb316f76
William Grant <williamg3575467@gmail.com>
2024-04-25 11:09:38 +0100

    Complete crud actions for List and create very basic unstyled views (for now)

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,37 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+
+  def to_root
+    redirect_to root_path
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # LISTS
+  root "lists#index"
+  get "lists", to: "lists#to_root" # Just redirect to the root path
+  get "lists/new", to: "lists#new"
+  get "lists/:id", to: "lists#show", as: :list
+  post "lists", to: "lists#create"
 end
