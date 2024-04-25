ac9422a0abdaa6b3204a405466456ed5d9f3a686
William Grant <williamg3575467@gmail.com>
2024-04-25 13:42:54 +0100

    Complete crud actions for bookmarks (but no styles still)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # LISTS
   root "lists#index"
   get "lists", to: "lists#to_root" # Just redirect to the root path
-  get "lists/new", to: "lists#new"
+
+  get "lists/new", to: "lists#new", as: :new_list
   get "lists/:id", to: "lists#show", as: :list
   post "lists", to: "lists#create"
+
+  get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :bookmarks
+  post "lists/:id/bookmarks", to: "bookmarks#create", as: :list_bookmarks
+  delete "/bookmark/:id", to: "bookmarks#delete", as: :delete_bookmark
 end
