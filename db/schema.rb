fcfacb4d144cacb574690be56855cc373139d1a0
William Grant <williamg3575467@gmail.com>
2024-04-25 10:09:41 +0100

    Create empty models and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_090342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "comment"
+    t.bigint "movies_id"
+    t.bigint "lists_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lists_id"], name: "index_bookmarks_on_lists_id"
+    t.index ["movies_id"], name: "index_bookmarks_on_movies_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "overview"
+    t.string "poster_url"
+    t.float "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookmarks", "lists", column: "lists_id"
+  add_foreign_key "bookmarks", "movies", column: "movies_id"
 end
